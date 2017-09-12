d3181fdeaa53d4dd42215b3fa46a207532acf959
pomfy113 <pomfy113@gmail.com>
2017-09-12 12:49:29 -0700

    Finished
    Everything including some extra stuff.
    Search engine, beautification, extra pages.

--- app.js
+++ app.js
@@ -2,36 +2,14 @@ var express = require('express');
 var app = express();
 var exphbs  = require('express-handlebars');
 var http = require('http');
+var giphy = require('giphy-api')();
 
-app.get('/', function (req, res) {
-  console.log(req.query.term)
-  var queryString = req.query.term;
-  // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
-  var term = encodeURIComponent(queryString);
-  // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
-  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
-
-  http.get(url, function(response) {
-    // SET ENCODING OF RESPONSE TO UTF8
-    response.setEncoding('utf8');
-
-    var body = '';
-
-    response.on('data', function(d) {
-      // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
-      body += d;
-    });
-
-    response.on('end', function() {
-      // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
-      var parsed = JSON.parse(body);
-      // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
-      res.render('home', {gifs: parsed.data})
-    });
-  });
-})
-
+// Use 'public folder'
+app.use(express.static('public'));
+// Use handlebars for layouts
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+
 app.set('view engine', 'handlebars');
 
 app.get('/hello-squirrel', function (req, res){
@@ -41,6 +19,10 @@ app.get('/hello-squirrel', function (req, res){
 app.get('/hello-gif', function (req, res) {
   var gifUrl = 'https://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
   res.render('hello-gif', {gifUrl: gifUrl})
+})
+
+app.get('/about', function(req, res){
+    res.render('about')
 })
 
 //app.get('/', function (req, res) {
@@ -55,4 +37,18 @@ app.get('/greetings/:name', function (req, res) {
 
 app.listen(3000, function () {
   console.log('Gif Search listening on port localhost:3000!');
+});
+
+app.get('/', function (req, res) {
+  giphy.search(req.query.term, function (err, response) {
+    if(response == undefined){
+        res.render('home')
+    }
+    else{
+    res.render('home', {gifs: response.data})
+    }
+  }
+
+
+);
 });
