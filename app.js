c7a15111db3efee9c979976b7c0251d6a8c18562
pomfy113 <pomfy113@gmail.com>
2017-09-20 16:19:57 -0700

    Improvements from review

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
+DEBUG=http:incoming,http:outgoing node index.js
+
+
 var express = require('express');
 var app = express();
 var exphbs  = require('express-handlebars');
 var http = require('http');
 var giphy = require('giphy-api')();
+var debug = require('debugger')();
 
 // Use 'public folder'
 app.use(express.static('public'));
 // Use handlebars for layouts
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-
-
 app.set('view engine', 'handlebars');
 
+// It's a squirrel!
 app.get('/hello-squirrel', function (req, res){
     res.send('Hello squirrel! Again! Again!');
 })
 
+// Greetings with a dog!
 app.get('/hello-gif', function (req, res) {
   var gifUrl = 'https://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
   res.render('hello-gif', {gifUrl: gifUrl})
 })
 
+// Extra page; basic about stuff
 app.get('/about', function(req, res){
     res.render('about')
 })
@@ -30,25 +35,30 @@ app.get('/about', function(req, res){
 //    res.render('home')
 //})
 
+// Grabs name from URL and adds it into greeting string
 app.get('/greetings/:name', function (req, res) {
     var name = req.params.name;
     res.render('greetings', {name: name});
 })
 
-app.listen(3000, function () {
-  console.log('Gif Search listening on port localhost:3000!');
+// Homepage giphy search engine
+app.get('/', function (req, res) {
+    // Sends response back to server for my sake
+    console.log(res)
+
+    // Giphy search API; grab the term and searches gifs related to it
+    // If no search term or first time booting, render home on its own
+    giphy.search(req.query.term, function (err, response) {
+        if(response == undefined){
+            res.render('home')
+        }
+    else{
+        res.render('home', {term: req.query.term, gifs: response.data})
+    }
+  });
 });
 
-app.get('/', function (req, res) {
-  giphy.search(req.query.term, function (err, response) {
-    if(response == undefined){
-        res.render('home')
-    }
-    else{
-    res.render('home', {gifs: response.data})
-    }
-  }
-
-
-);
+// Listening function
+app.listen(3000, function () {
+  console.log('Gif Search listening on port localhost:3000!');
 });
